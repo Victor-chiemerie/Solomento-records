ea21e3634d24eef9816e9f2f1caa724f5009b072
Victor-chiemerie <victorchiemerie02@gmail.com>
2024-10-29 06:22:46 +0100

    Setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+    // Initialize Firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
   runApp(const MyApp());
 }
 
