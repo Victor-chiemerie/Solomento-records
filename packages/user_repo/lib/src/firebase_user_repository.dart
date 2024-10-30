86b9a0ccc62fda8ef376909038b742bf1bf3085a
Victor-chiemerie <victorchiemerie02@gmail.com>
2024-10-30 02:19:15 +0100

    setup firebase authentication methods for users

--- packages/user_repo/lib/src/firebase_user_repository.dart
+++ packages/user_repo/lib/src/firebase_user_repository.dart
@@ -1,5 +1,67 @@
+import 'dart:developer';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/my_user.dart';
 import 'user_repo.dart';
 
 class FirebaseUserRepository implements UserRepository {
-  
+  FirebaseUserRepository({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  final FirebaseAuth _firebaseAuth;
+
+  // Sign Up
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: myUser.email,
+        password: password,
+      );
+
+      // set the ID of the created user to the ID gotten from firebase
+      myUser = myUser.copyWith(id: user.user!.uid);
+
+      return myUser;
+    } catch (error) {
+      log(error.toString());
+      rethrow;
+    }
+  }
+
+  // Sign In
+  @override
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (error) {
+      log(error.toString());
+      rethrow;
+    }
+  }
+
+  // Sign Out
+  @override
+  Future<void> logOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (error) {
+      log(error.toString());
+      rethrow;
+    }
+  }
+
+  // Reset Password
+  @override
+  Future<void> resetPassword(String email) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (error) {
+      log(error.toString());
+      rethrow;
+    }
+  }
 }
--- packages/user_repo/lib/src/user_repo.dart
+++ packages/user_repo/lib/src/user_repo.dart
@@ -1,3 +1,15 @@
+import '../user_repository.dart';
+
 abstract class UserRepository {
-  
+  /// Sign in method
+  Future<void> signIn(String email, String password);
+
+  /// Sign out method
+  Future<void> logOut();
+
+  /// Sign up method
+  Future<MyUser> signUp(MyUser myUser, String password);
+
+  /// Reset password method
+  Future<void> resetPassword(String email);
 }
