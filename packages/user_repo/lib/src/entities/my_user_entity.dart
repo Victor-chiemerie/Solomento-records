9746c2709a5eb8597a461e8462f1a743ff9c6731
Victor-chiemerie <victorchiemerie02@gmail.com>
2024-10-30 01:50:38 +0100

    create user and user entity models for project

--- packages/user_repo/lib/src/entities/my_user_entity.dart
+++ packages/user_repo/lib/src/entities/my_user_entity.dart
@@ -1,3 +1,44 @@
-class MyUserEntity {
-  
+import 'package:equatable/equatable.dart';
+
+class MyUserEntity extends Equatable {
+  // entity paramenters
+  final String id;
+  final String email;
+  final String name;
+
+  const MyUserEntity({
+    required this.id,
+    required this.email,
+    required this.name,
+  });
+
+  /// Converts the user entity to a document to be stored to firebase
+  Map<String, Object> toDocument() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+    };
+  }
+
+  /// Converts a document from firebase to a user entity
+  static MyUserEntity fromDocument(Map<String, Object> doc) {
+    return MyUserEntity(
+      id: doc['id'] as String,
+      email: doc['email'] as String,
+      name: doc['name'] as String,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''UserEntity: {
+      id: $id
+      email: $email
+      name: $name
+    }''';
+  }
+
+  @override
+  List<Object?> get props => [id, email, name];
 }
--- packages/user_repo/lib/src/models/my_user.dart
+++ packages/user_repo/lib/src/models/my_user.dart
@@ -1,3 +1,61 @@
-class MyUser {
-  
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:user_repository/src/entities/entities.dart';
+
+class MyUser extends Equatable {
+  // user paramenters
+  final String id;
+  final String email;
+  final String name;
+
+  const MyUser({
+    required this.id,
+    required this.email,
+    required this.name,
+  });
+
+  /// Empty user which represents an unauthenticated user
+  static const empty = MyUser(
+    id: "",
+    email: "",
+    name: "",
+  );
+
+  /// Modify MyUser parameters
+  MyUser copyWith({
+    String? id,
+    String? email,
+    String? name,
+  }) {
+    return MyUser(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+    );
+  }
+
+  /// Convenience getter to determine whether the current user is empty
+  bool get isEmpty => this == MyUser.empty;
+
+  /// Convenience getter to determine whether the current user is not empty
+  bool get isNotEmpty => this != MyUser.empty;
+
+  MyUserEntity toEntity() {
+    return MyUserEntity(
+      id: id,
+      email: email,
+      name: name,
+    );
+  }
+
+  static MyUser fromEntity(MyUserEntity entity) {
+    return MyUser(
+      id: entity.id,
+      email: entity.email,
+      name: entity.name,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name];
 }
